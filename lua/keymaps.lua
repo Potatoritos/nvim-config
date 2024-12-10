2eb376bbd9b20b558bda66a1cbcd9f0d182f9593
Potatoritos <32649997+Potatoritos@users.noreply.github.com>
2024-12-10 04:48:19 -0500

    Add keymaps for toggling comments/colorizer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,6 +10,10 @@ vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Focus upper window' })
 vim.keymap.set('n', '<leader>f', '<cmd>HopWord<CR>', { desc = 'Hop to word' })
 vim.keymap.set('n', '<leader>p', '<cmd>HopPattern<CR>', { desc = 'Hop to pattern' })
 
+vim.keymap.set('n', '<leader>c', '<cmd>ColorizerToggle<CR>', { desc = 'Toggle colorizer' })
+vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle comment', remap = true })
+vim.keymap.set('v', '<leader>/', '<ESC>gvgc', { desc = 'Toggle comment', remap = true })
+
 vim.keymap.set('n', '<F1>', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>')
 vim.keymap.set('n', '<F2>', '<cmd>Telescope buffers<CR>')
 
