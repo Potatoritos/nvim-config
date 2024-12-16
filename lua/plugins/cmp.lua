bc449fd9c7fe3e63082a7cbf42f9cd722a98d146
Potatoritos <potatoritos@gmail.com>
2024-12-16 08:09:06 -0500

    Replace nvim-cmp with blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,104 +1,98 @@
 local kind_icons = {
-    Class = '󰠱',
-    Color = '󰏘',
-    Constant = '󰏿',
-    Constructor = '󰾍',
-    Enum = '',
-    EnumMember = '',
-    Event = '',
-    Field = '󰫧',
-    File = '󰈙',
-    Folder = '󰉋',
-    Function = '󰊕',
-    Interface = '',
-    Keyword = '󰌋',
-    Method = '󰡱',
-    Module = '󰆧',
-    Operator = '󰦒',
-    Property = '󰜢',
-    Reference = '󰪍',
-    Snippet = '󰗀',
-    Struct = '󰅩',
-    Text = '󰦨',
-    TypeParameter = '󰠲',
-    Unit = '',
-    Value = '󰎠',
-    Variable = '󰫧',
+    Class = ' 󰆧 ',
+    Color = ' 󰏘 ',
+    Constant = ' 󰏿 ',
+    Constructor = ' 󰡱 ',
+    Enum = '  ',
+    EnumMember = '  ',
+    Event = '  ',
+    Field = ' 󰫧 ',
+    File = '  ',
+    Folder = ' 󰉋 ',
+    Function = ' 󰊕 ',
+    Interface = ' 󱐝 ',
+    Keyword = ' 󰌋 ',
+    Method = ' 󰊕 ', -- ' 󰡱 ',
+    Module = ' 󰆦 ',
+    Operator = ' 󰦒 ',
+    Property = ' 󰜢 ',
+    Reference = '  ', -- ' 󰪍 ',
+    Snippet = ' 󰗀 ', -- ' 󰨾 ',
+    Struct = ' 󰆧 ',
+    Text = ' 󰦨 ',
+    TypeParameter = ' 󰬛 ',
+    Unit = '  ',
+    Value = ' 󰎠 ',
+    Variable = ' 󰫧 ',
 }
 
-local source_icons = {
-    nvim_lsp = 'Lsp',
-    buffer = 'Buf',
-    path = 'Path',
-    calc = 'Calc',
-}
-
-local opts = function()
-    local cmp = require('cmp')
-    return {
-        mapping = {
-            ['<C-j>'] = cmp.mapping.select_next_item(),
-            ['<C-k>'] = cmp.mapping.select_prev_item(),
-            ['<Tab>'] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Insert,
-                select = true,
-            }),
-            ['<C-u>'] = cmp.mapping.scroll_docs(-8),
-            ['<C-d>'] = cmp.mapping.scroll_docs(8),
-        },
-        sources = {
-            { name = 'path' },
-            { name = 'nvim_lsp' },
-            { name = 'nvim_lsp_signature_help' },
-            { name = 'nvim_lua' },
-            { name = 'buffer' },
-            { name = 'calc' },
-            {
-                name = 'lazydev',
-                group_index = 0,
-            },
-        },
-        view = {
-            entries = {
-                name = 'custom',
-                selection_order = 'near_cursor',
-            },
-        },
-        window = {
-            completion = {
-                winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
-                side_padding = 0,
-            },
-        },
-        formatting = {
-            fields = { 'kind', 'abbr', 'menu' },
-            format = function(entry, item)
-                item.menu = item.kind
-                item.kind = string.format(' %s ', kind_icons[item.kind])
-                return item
-            end
-        },
-    }
-end
+-- 󱥌 󱥒 󱥔 󱥍 󱥏 󱥎 
 
 return {
-    {
-        'hrsh7th/nvim-cmp',
-        opts = opts,
+    'saghen/blink.cmp',
+    lazy = false,
+    version = 'v0.*',
+    opts = {
+        keymap = {
+            ['<C-j>'] = { 'select_next', 'fallback' },
+            ['<C-k>'] = { 'select_prev', 'fallback' },
+            ['<C-l>'] = { 'snippet_forward', 'fallback' },
+            ['<C-h>'] = { 'snippet_backward', 'fallback' },
+            ['<Tab>'] = { 'select_and_accept', 'fallback' },
+            ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+            ['<C-e>'] = { 'hide', 'show', 'fallback' },
+        },
+        sources = {
+            default = {
+                'lsp',
+                'path',
+                'snippets',
+                'buffer',
+                'lazydev',
+            },
+        },
+        completion = {
+            list = {
+                selection = 'manual',
+            },
+            accept = {
+                create_undo_point = true,
+                auto_brackets = {
+                    enabled = true,
+                },
+                menu = {
+                    enabled = true,
+                    max_height = 10,
+
+                }
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 300,
+                update_delay_ms = 50,
+                treesitter_highlighting = true,
+            },
+            ghost_text = {
+                enabled = false,
+            },
+            menu = {
+                draw = {
+                    padding = { 0, 1 },
+                    columns = {
+                        { 'kind_icon' },
+                        { 'label', 'label_description', gap = 1 },
+                    },
+                },
+            },
+        },
+        fuzzy = {
+            use_typo_resistance = false,
+        },
+        appearance = {
+            kind_icons = kind_icons,
+        },
     },
-    {
-        'hrsh7th/cmp-nvim-lsp'
-    },
-    {
-        'hrsh7th/cmp-nvim-lsp-signature-help'
-    },
-    {
-        'hrsh7th/cmp-path'
-    },
-    {
-        'hrsh7th/cmp-buffer'
-    },
-    {
-        'hrsh7th/cmp-calc'
-    },
+    opts_extend = { "sources.default" },
 }
+
