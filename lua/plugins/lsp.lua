0676e650b7b324775e6ec48173f7c4d8286e6491
Potatoritos <potatoritos@gmail.com>
2024-12-19 20:56:37 -0500

    Disable LSP snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,16 @@
--- vim.g.rustaceanvim = {
---     server = {
---         capabilities = {
---             textDocument = {
---                 completion = {
---                     completionItem = {
---                         snippetSupport = false,
---                     },
---                 },
---             },
---         },
---     },
--- }
+vim.g.rustaceanvim = {
+    server = {
+        capabilities = {
+            textDocument = {
+                completion = {
+                    completionItem = {
+                        snippetSupport = false,
+                    },
+                },
+            },
+        },
+    },
+}
 
 return {
     {
@@ -21,7 +21,13 @@ return {
         },
         config = function()
             local capabilities = require('blink.cmp').get_lsp_capabilities()
-            -- capabilities.textDocument.completion.completionItem.snippetSupport = false
+            capabilities.textDocument.completion.completionItem.snippetSupport = false
+
+            -- disable lua_ls snippets
+            ---@diagnostic disable-next-line inject-field
+            capabilities.textDocument.completion.callSnippet = 'Disable'
+            ---@diagnostic disable-next-line inject-field
+            capabilities.textDocument.completion.keywordSnippet = 'Disable'
 
             local lsp = require('lspconfig')
             lsp.lua_ls.setup({ capabilities = capabilities })
