31403d5487734333b7146606bfb78ddb055824e3
Potatoritos <potatoritos@gmail.com>
2025-01-05 18:17:33 -0500

    Automatically install formatters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,35 +1,45 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-        {
-            '<leader>lf',
-            function()
-                require('conform').format({ async = true })
-            end,
-            mode = 'n',
-            desc = 'Format buffer',
+    {
+        'stevearc/conform.nvim',
+        event = { 'BufWritePre' },
+        cmd = { 'ConformInfo' },
+        keys = {
+            {
+                '<leader>lf',
+                function()
+                    require('conform').format({ async = true })
+                end,
+                mode = 'n',
+                desc = 'Format buffer',
+            },
         },
+        ---@module 'conform'
+        ---@type conform.setupOpts
+        opts = {
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                python = { 'isort', 'black' },
+                javascript = { 'prettierd' },
+                typescript = { 'prettierd' },
+            },
+            default_format_opts = {
+                lsp_format = 'fallback',
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = 'never',
+            },
+        },
+        init = function()
+            vim.o.formatexpr = 'v:lua.require("conform").formatexpr()'
+        end,
     },
-    ---@module 'conform'
-    ---@type conform.setupOpts
-    opts = {
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            python = { 'isort', 'black' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
-            typescript = { 'prettierd', 'prettier', stop_after_first = true },
+    {
+        'zapling/mason-conform.nvim',
+        dependencies = {
+            'stevearc/conform.nvim',
+            'williamboman/mason.nvim',
         },
-        default_format_opts = {
-            lsp_format = 'fallback',
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_format = 'never',
-        },
+        opts = {},
     },
-    init = function()
-        vim.o.formatexpr = 'v:lua.require("conform").formatexpr()'
-    end,
 }
