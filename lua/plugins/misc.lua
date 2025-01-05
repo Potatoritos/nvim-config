c15f3f0360e6e32b78578c8f825c011ab75c3d4e
Potatoritos <32649997+Potatoritos@users.noreply.github.com>
2025-01-04 21:23:07 -0500

    Add matrix snippet

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -65,6 +65,9 @@ return {
     {
         'lervag/vimtex',
         lazy = false,
+        config = function()
+            vim.g.vimtex_syntax_enabled = 0
+        end
     },
     {
         'nyoom-engineering/oxocarbon.nvim'
--- snippets/tex.lua
+++ snippets/tex.lua
@@ -104,7 +104,30 @@ local wrap = function(trig, left, right)
     })
 end
 
-local snippets = {
+local split_char = function(str, char)
+    local res = {}
+    local token = {}
+    local index = 1
+
+    while index <= #str do
+        local c = string.sub(str, index, index)
+        if c == char then
+            table.insert(res, table.concat(token, ''))
+            token = {}
+        else
+            table.insert(token, c)
+        end
+
+        if index == #str then
+            table.insert(res, table.concat(token, ''))
+        end
+
+        index = index + 1
+    end
+    return res
+end
+
+return {
     s_split(';f', '\\frac{%s}{%s}'),
     s_split(';li', '\\lim{%s \\to %s}'),
     s_split(';it', '\\int_{%s}^{%s}'),
@@ -129,6 +152,58 @@ local snippets = {
     wrap(';bb', '\\mathbb{', '}'),
     wrap(';ab', '\\left\\lvert', '\\right\\rvert'),
     wrap(';nr', '\\left\\lVert', '\\right\\rVert'),
-}
+    postfix({ trig = ';ma', snippetType = 'autosnippet', match_pattern = '^.*$' }, {
+        f(function(_, parent)
+            local match = parent.snippet.env.POSTFIX_MATCH
+            local spl = split_char(match, ' ')
 
-return snippets
+            print(vim.inspect(spl))
+
+            local info = table.remove(spl, #spl)
+            local index = 1
+
+            local cols
+            if string.match(info, '^%d') then
+                cols = tonumber(string.sub(info, 1, 1))
+                index = index + 1
+            else
+                cols = 1
+            end
+
+            local rows = math.floor(#spl / cols)
+
+            local type
+            if string.match(info, '^%a', index) then
+                type = string.sub(info, index, index)
+            else
+                type = 'b'
+            end
+
+            local res = { '\\begin{' .. type .. 'matrix}' }
+            local line = nil
+
+            for i = 1, rows*cols do
+                local word = spl[i]
+                if word == nil then
+                    word = ''
+                end
+                if line == nil then
+                    line = '\t' .. word
+                else
+                    line = line .. ' & ' .. word
+                end
+
+                if i % cols == 0 then
+                    if i ~= rows * cols then
+                        line = line .. ' \\\\'
+                    end
+                    table.insert(res, line)
+                    line = nil
+                end
+            end
+
+            table.insert(res, '\\end{' .. type .. 'matrix}')
+            return res
+        end),
+    }),
+}
